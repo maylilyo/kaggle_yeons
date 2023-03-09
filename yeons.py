b0b885466c7925719319c47c5fd93e6ce06ffaad
maylilyo <maylily_o@naver.com>
2023-03-09 16:38:24 +0900

    EDIT : isna/sum

--- yeons.py
+++ yeons.py
@@ -27,11 +27,6 @@ from modules.utils import set_all_seeds
 from modules.dataset import get_data
 from modules.regressor import CustomRegressor
 
-# Visualization library(Not use currently)
-from matplotlib import pyplot as plt, rcParams, style
-import seaborn as sns
-from plotly import express as px, graph_objects as go
-
 
 def main(args):
     device = torch.device(
@@ -53,47 +48,11 @@ def main(args):
     ypred_svr = pd.DataFrame(svr.predict(xtest), index = xtest.index, columns = y.columns).clip(0.)
 
     yfit_mean = pd.DataFrame(np.mean([yfit_svr.values, yfit_lnr.values], axis = 0), index = x.index, columns = y.columns).clip(0.)
-    ypred_mean = pd.DataFrame(np.mean([ypred_lnr.values, ypred_svr.values], axis = 0), index = xtest.index, columns = y.columns).clip(0.)
 
     y_ = y.stack(['store_nbr', 'family'])
     y_['lnr'] = yfit_lnr.stack(['store_nbr', 'family'])['sales']
     y_['svr'] = yfit_svr.stack(['store_nbr', 'family'])['sales']
     y_['mean'] = yfit_mean.stack(['store_nbr', 'family'])['sales']
-
-    print('='*70, 'Linear Regression', '='*70)
-    print(y_.groupby('family').apply(lambda r : np.sqrt(msle(r['sales'], r['lnr']))))
-    print('LNR RMSLE :', np.sqrt(msle(y, yfit_lnr)))
-    print('='*70, 'SVR', '='*70)
-    print(y_.groupby('family').apply(lambda r : np.sqrt(msle(r['sales'], r['svr']))))
-    print('SVR RMSLE :', np.sqrt(msle(y, yfit_svr)))
-    print('='*70, 'Mean', '='*70)
-    print(y_.groupby('family').apply(lambda r : np.sqrt(msle(r['sales'], r['mean']))))
-    print('Mean RMSLE :', np.sqrt(msle(y, yfit_mean)))
-
-
-    #
-    print('='*70, 'Linear Regression', '='*70)
-    print(y_.groupby('family').apply(lambda r : mae(r['sales'], r['lnr'])))
-    print('LNR MAE :', mae(y, yfit_lnr))
-    print('='*70, 'SVR', '='*70)
-    print(y_.groupby('family').apply(lambda r : mae(r['sales'], r['svr'])))
-    print('SVR MAE :', mae(y, yfit_svr))
-    print('='*70, 'Mean', '='*70)
-    print(y_.groupby('family').apply(lambda r : mae(r['sales'], r['mean'])))
-    print('Mean MAE :', mae(y, yfit_mean))
-
-    #
-    true_low = [2]
-    pred_low = [4]
-
-    print('RMSLE for low value :', np.sqrt(msle(true_low, pred_low)))
-    print('MAE for low value :', mae(true_low, pred_low))
-
-    true_high = [255]
-    pred_high = [269]
-
-    print('RMSLE for high value :', np.sqrt(msle(true_high, pred_high)))
-    print('MAE for high value :', mae(true_high, pred_high))
 
     # Fit Progress
     ymean = yfit_lnr.append(ypred_lnr)
@@ -104,13 +63,14 @@ def main(args):
     xtest = xtest.join(ymean)
 
     y = y.loc['2017-05-01':]
+    print(y.isna().sum().sum())
 
     model = CustomRegressor(n_jobs=-1, verbose=1, seed=args.seed)
     model.fit(x, y)
 
-
     # Predict Progress
     y_pred = pd.DataFrame(model.predict(x), index=x.index, columns=y.columns)
+    print(y_pred.isna().sum().sum())
     y_pred = y_pred.stack(['store_nbr', 'family']).clip(0.)
     y_ = y.stack(['store_nbr', 'family']).clip(0.)
 
@@ -118,14 +78,14 @@ def main(args):
     print(y_.groupby('family').apply(lambda r : np.sqrt(np.sqrt(mean_squared_log_error(r['sales'], r['pred'])))))
 
     # Looking at error
-    print('RMSLE : ', np.sqrt(np.sqrt(msle(y_['sales'], y_['pred']))))
+    print('RMSLE : ', np.sqrt(np.sqrt(msle(y_['sales'], y_['pred'])))) # RMSLE 값이 약간 다름
     y_pred.isna().sum()
 
     ypred = pd.DataFrame(model.predict(xtest), index = xtest.index, columns = y.columns).clip(0.)
     ypred = ypred.stack(['store_nbr', 'family'])
     sub = pd.read_csv(f'{args.datapath}/sample_submission.csv')
     sub['sales'] = ypred.values
-    sub.to_csv('submission7.csv', index = False) # Submit
+    sub.to_csv(f'{os.getcwd()}/result/submission{args.submission_name}_remake.csv', index = False) # Submit
 
 
 if __name__ == "__main__":
